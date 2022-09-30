34953628a23df5dc34b3eeb1ef164538435bbd8a
Pabearyoga <pabearyoga@gmail.com>
2022-10-01 00:57:49 +0300

    fix Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -12,7 +12,7 @@ const Modal = ({largeIMG, onClose}) => {
       window.addEventListener('click', handelCloseModal);
     }
 
-  }, [() => handelCloseModal]);
+  }, [ onClose]);
 
   const handelCloseModal = event => {
     if (event.code === 'Escape' || event.target.id === 'Overlay') {
