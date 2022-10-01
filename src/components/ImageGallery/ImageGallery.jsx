ac9e6057ca9620d0abfa1df7afb4704b653eaf4c
Pabearyoga <pabearyoga@gmail.com>
2022-10-02 01:45:40 +0300

    fix pagination

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -19,7 +19,7 @@ const STATUS = {
 const ImageGallery = ({query}) => {
   const [images, setImages] = useState([]);
   const [totalHits, setTotalHits] = useState(null);
-  const [tags, setTags] = useState('');
+  const [tags] = useState('');
   const [page, setPage] = useState(1);
   const [largeIMG, setLargeIMG] = useState(null);
   const [status, setStatus] = useState(STATUS.idle);
